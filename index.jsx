06668378b668000ec962d11ee6caee8ef963dd71
kolychka <randytaylor.fckoff@gmail.com>
2020-08-23 20:46:28 +0500

    Inital commit, added start struct

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+import './style.scss'
+
+function App() {
+    return (
+        <div className="wrapper">
+            
+        </div>
+    )
+}
+
+render(<App />, document.getElementById('app'));
